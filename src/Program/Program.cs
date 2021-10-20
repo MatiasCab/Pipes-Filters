535ba41f83572b9728396a8856c0fb5c13cfabf6
Matias <MATIAS.CABRERA@correo.ucu.edu.uy>
2021-10-20 11:22:58 -0300

    Se completaron los ejercicios

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using CompAndDel.Pipes;
 using CompAndDel.Filters;
+using TwitterUCU;
 
 namespace CompAndDel
 {
@@ -8,6 +9,21 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+
+            PictureProvider provider = new PictureProvider();
+            IPicture picture1 = provider.GetPicture(@".\beer.jpg");//".\" para decir que estoy parado en esta carpeta
+
+            IFilter grey = new FilterGreyscale();
+            IFilter negative = new FilterNegative();
+
+            PipeNull pipeNull = new PipeNull();
+            PipeSerial pipe2 = new PipeSerial(negative,pipeNull);
+            PipeSerial pipe1 = new PipeSerial(grey,);
+            PipeConditional conditional= new PipeConditional(pipe1,pipe2);
+            
+            conditional.ContainsFace(@".\beer.jpg");
+
+            conditional.Send(picture1);
         }
     }
 }
